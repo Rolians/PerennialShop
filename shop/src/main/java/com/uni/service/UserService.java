4808fcee756e9ffee71e94316bf29fa240d1e911
Rolians <35889882+Rolians@users.noreply.github.com>
2019-02-20 11:53:32 +0530

    Update UserService.java

--- shop/src/main/java/com/uni/service/UserService.java
+++ shop/src/main/java/com/uni/service/UserService.java
@@ -1,43 +1,35 @@
 package com.uni.service;
 
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.uni.dao.UserDao;
 
+@Service
 public class UserService
 {
 	@Autowired
 	UserDao userDao;
 	
 	//Authenticate provided credentials
-	public boolean login(String UserName,String Password) throws Exception 
+	public boolean login(String UserName,String Password) throws SQLException, NullPointerException 
 	{
-		try {
 			if(userDao.checkUser(UserName,Password)) {
 				return true;
 			}else {
 				return false;
 			}
-		}catch(Exception e) {
-			throw e;
-		}
 	}
 	
 	//Return UserId of logged user
 	public int getUserId(String UserName,String Password) throws Exception {
-		try {
-			return userDao.getUserId(UserName, Password);
-		}catch(Exception e) {
-			throw e;
-		}
+		return userDao.getUserId(UserName, Password);
 	}
 	
 	//Return unique cartId assigned to specified user to add into session
 	public int getCartId(int userId) throws Exception {
-		try {
 			return userDao.getCartId(userId);
-		}catch(Exception e) {
-			throw e;
-		}
 	}
 }
